37cc02fd49f903ab0fd8c5979ad8d29876276d65
atcdo <bendod2020@gmail.com>
2024-01-26 22:47:56 -0800

    HELP

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,12 @@
-public class Main {
+public class Main implements MemberEncyclopedia{
     public static void main(String[] args) {
-
+        JDABuilder builder = JDABuilder.createDefault(args[0]);
+        JDA jda = JDABuilder.createDefault("MTA1MDI0MTQ1MTY3NTYzNTc0Mw.GeXOLa.poA_k437aUNxzZz7APuQyaqsVthoqsT8rJk_AI").awaitReady().build();
+        List<Member> members = guild.getMembersWithRoles(roles);
+        System.out.println(members);
     }
+
+//    public void memberList(){
+//        List<Member> members = guild.getMembersWithRoles(roles);
+//    }
 }
---
+++ src/main/java/MemberEncyclopedia.java
@@ -0,0 +1,9 @@
+public class MemberEncyclopedia{
+    public String name = "";
+    public int cash = 0;
+    public void memberList(String name, int cash){
+        this.name = name;
+        this.cash = cash;
+        //*TODO* https://stackoverflow.com/questions/61288928/java-discord-bot-get-members-of-a-role
+    }
+}
