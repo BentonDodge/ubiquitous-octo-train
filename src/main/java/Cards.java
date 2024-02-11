0590c240619b33aaef719e42c86cc9ec8909c1fc
atcdo <bendod2020@gmail.com>
2024-02-10 22:58:47 -0800

    Changed specific card from method to variable

--- src/main/java/Cards.java
+++ src/main/java/Cards.java
@@ -1,7 +1,5 @@
 public class Cards {
     public static int Number = (int) (Math.random()*12)+1;
     public static int Suit = (int) (Math.random()*3)+1;
-    public static int Cards(){
-        return Number*Suit;
-    }
+    public static int Cards = Number*Suit;
 }
